72cf342013460c4b784327e47cc6e3dc1f3d20c8
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-25 22:02:09 +0300

    Initial commit

---
+++ İlk Harf Buyuk/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace İlk_Harf_Buyuk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(168, 21);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(232, 21);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+           
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(23, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Adınızı Soyadınızı Yazınız:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(416, 61);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "İlk Harf Büyük";
+           
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ İlk Harf Buyuk/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace İlk_Harf_Buyuk
+{
+    public partial class Form1 : Form
+    {
+        #region
+
+        int a;
+
+        string cümle;
+
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+
+            cümle = textBox1.Text;
+
+            if (cümle.Length == 1)
+            {
+               textBox1.Text = cümle.ToUpper();
+               
+
+                textBox1.SelectionStart = textBox1.TextLength;
+                textBox1.Focus();
+            }
+
+            else if (cümle.Length - 1 == a)
+            {
+                cümle = cümle.Substring(0, a) + cümle.Substring(a-1).ToUpper();
+                textBox1.Text = cümle;
+                a = 0;
+
+                textBox1.SelectionStart = textBox1.TextLength;
+                textBox1.Focus();
+            }
+
+            else if (cümle.Length==a)
+            {
+                textBox1.Text = cümle;
+            }
+
+       }
+
+        
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                
+                a = textBox1.Text.Length+1;
+                textBox1.SelectionStart = textBox1.TextLength;
+                textBox1.Focus();
+                textBox1.Enabled = true;
+
+            }
+        }
+
+      
+    }
+}
